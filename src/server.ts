6c0329c0c4988574e1f8752befbec7910be4ca7f
Jordan <brasil@xpdeal.com>
2024-10-25 02:00:10 -0300

    bk

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer } from "./server/WebSocketServer";
 
 const server = new WebSocketServer();
-const serverInstance = server.start(3001);
+const serverInstance = server.start(3000);
 
 console.log(`WebSocket server running on ws://localhost:${serverInstance.port}`);
